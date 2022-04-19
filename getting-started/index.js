9c0d6783a3347dd48fb295ea947f1b43431e5343
okyungjin <okyungjin@gmail.com>
2022-04-19 17:27:07 +0900

    tsc 사용하여 ts 파일을 js 파일로 컴파일하기

---
+++ getting-started/index.js
@@ -0,0 +1,4 @@
+function sumNumbers(a, b) {
+    return a + b;
+}
+sumNumbers(10, 20);
---
+++ getting-started/index.ts
@@ -0,0 +1,5 @@
+function sumNumbers(a: number, b: number): number {
+    return a + b;
+}
+
+sumNumbers(10, 20);
